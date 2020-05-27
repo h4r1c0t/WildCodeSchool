2107e1f5a791d938a6da286aebf0e10a6f5bcbf2
h4r1c0t <vanstavel.sebastien@gmail.com>
2020-05-27 14:47:27 +0200

    Update ActorsData.py

--- Project/#3_movies_recommandations/Scripts/ActorsData.py
+++ Project/#3_movies_recommandations/Scripts/ActorsData.py
@@ -1,4 +1,4 @@
-#%% MODULES IMPORTATIONS
+# %% MODULES IMPORTATIONS
 import sqlite3
 import numpy as np
 import pandas as pd
@@ -11,12 +11,53 @@ import streamlit as st
 import requests
 import json
 
-#%% CONNECTION TO DB
+# %% CONNECTION TO DB
 path = 'D:/tmp storage/Project#3/SQLite3 db/GrosseBertha_1.2.db'
 conn = sqlite3.connect(path)
 
-#%% Fonction de création d'un BDD avec les acteurs pour chaque films (provient du code de Coralie).
-def acteurs_films() :
+#################### RECUPERATION DES DONNEES SUR LES ACTEURS ####################
+# %% Création d'une BDD avec les infos de chaques acteurs
+def acteurs():
+        df_acteurs = pd.read_sql_query(
+                '''SELECT *
+                    FROM imdb_name_basic inb
+                    ''', conn)
+
+        # Créé une liste avec les id des films pour lequels l'acteur est connu.
+        df_acteurs['knownForTitles'] = df_acteurs['knownForTitles'].apply(lambda x: x.split(','))
+
+        # Remplace les id des films par leurs titres.
+        ## Créé le DataFrame contenant les films
+        df_films = pd.read_sql_query(
+                '''SELECT tconst idFilm, originalTitle title, genres
+                    FROM imdb_title_basics itb
+                    ''', conn)
+
+        return (df_acteurs)
+
+
+df_acteurs = acteurs()
+
+# %% Création d'un DF avec les films
+def films():
+        df_films = pd.read_sql_query(
+                '''SELECT   m.movieId movieId, tab1.imdbId imdbId, m.title clientTitle, tab1.imdbTitle title, tab1.durée, m.genres genres
+                    FROM movies m 
+                    JOIN (SELECT l.movieId movieId, imdbId, itb.primaryTitle imdbTitle, startYear year, runtimeMinutes durée
+                           FROM imdb_title_basics itb
+                           JOIN links l
+                           ON l.imdbId = itb.tconst
+                           ) tab1
+                    ON tab1.movieId = m.movieId
+                    ''', conn)
+        return df_films
+
+
+df_films = films()
+
+#################### NOMBRE DE FILMS PAR ACTEURS ####################
+# %% Fonction de création d'un BDD avec les acteurs pour chaque films (provient du code de Coralie).
+def acteurs_films():
         df_acteurs = pd.read_sql_query(
                 '''SELECT   itp.tconst,
                             itp.ordering,
@@ -58,77 +99,56 @@ def acteurs_films() :
         df_acteurs = pd.merge(df_acteurs, df_acteur4, on = 'imdbId', how = 'outer')
         return (df_acteurs)
 
-#%% Création d'un DataFrame avec les acteurs pour chaque film
 df_acteurs_films = acteurs_films()
-df_acteurs_films.info()
 
-#%% Création d'une BDD avec les infos de chaques acteurs
-def acteurs():
-        df_acteurs = pd.read_sql_query(
-                '''SELECT *
-                    FROM imdb_name_basic inb
-                    ''', conn)
+# %% Création d'un DF en long avec les films et leurs acteurs
+acteur1 = df_acteurs_films[['imdbId', 'acteur1']]; acteur1.rename(columns = {'acteur1': 'acteur'}, inplace = True)
+acteur2 = df_acteurs_films[['imdbId', 'acteur2']]; acteur2.rename(columns = {'acteur2': 'acteur'}, inplace = True)
+acteur3 = df_acteurs_films[['imdbId', 'acteur3']]; acteur3.rename(columns = {'acteur3': 'acteur'}, inplace = True)
+acteur4 = df_acteurs_films[['imdbId', 'acteur4']]; acteur4.rename(columns = {'acteur4': 'acteur'}, inplace = True)
 
-        # Créé une liste avec les id des films pour lequels l'acteur est connu.
-        df_acteurs['knownForTitles'] = df_acteurs['knownForTitles'].apply(lambda x: x.split(','))
+# DF long acteurs | films
+df_acteurs_imdbId = pd.concat([acteur1, acteur2, acteur3, acteur4])
+df_acteurs_imdbId.dropna(inplace = True)
 
-        # Remplace les id des films par leurs titres.
-        ## Créé le DataFrame contenant les films
-        df_films = pd.read_sql_query(
-                '''SELECT tconst idFilm, originalTitle title, genres
-                    FROM imdb_title_basics itb
-                    ''', conn)
+# DF NbFilms par acteur.
+df_nb_films_acteurs = df_acteurs_imdbId.groupby(by = 'acteur').count()
+df_nb_films_acteurs.reset_index(inplace = True)
+df_nb_films_acteurs.rename(columns = {'imdbId': 'nb_film'}, inplace = True)
 
-        return (df_acteurs)
-
-
-#%%
-df_acteurs = acteurs()
-
-#%% Création d'un DF avec les films
-def films():
-        df_films = pd.read_sql_query(
-                '''SELECT   m.movieId movieId, tab1.imdbId imdbId, m.title clientTitle, tab1.imdbTitle title, tab1.durée, m.genres genres
-                    FROM movies m 
-                    JOIN (SELECT l.movieId movieId, imdbId, itb.primaryTitle imdbTitle, startYear year, runtimeMinutes durée
-                           FROM imdb_title_basics itb
-                           JOIN links l
-                           ON l.imdbId = itb.tconst
-                           ) tab1
-                    ON tab1.movieId = m.movieId
-                    ''', conn)
-        return df_films
-
-
-df_films = films()
-
-#%% Fonction de sélection d'un acteur
-def actor_select(actor='Nicolas Cage'):
+#
+#################### RECHERCHE DES INFOS D'UN ACTEUR ####################
+# %% Fonction de sélection d'un acteur
+def actor_select(actor = 'Nicolas Cage'):
         actors_list = list(df_acteurs['primaryName'])
 
-        # Cette boucle if permet de rechercher la bonne orthographe
+        # Boucle if: si la syntaxe n'est pas bonne, alors on va chercher le nom le plus proche.
         if actor in actors_list:
                 pass
         else:
                 actor = process.extractOne(actor, actors_list)[0]
 
-
+        # Infos de base sur l'acteur
         actor_infos = df_acteurs[df_acteurs['primaryName'] == actor]
         actor_name = actor_infos.iloc[0, 1]
         actor_dates = actor_infos.iloc[0, 2:3]
         actor_prof = actor_infos.iloc[0, 4].split(",")
         actor_prof1 = actor_prof[0]
         actor_movies = actor_infos.iloc[0, 5]
+        movies_title_list = []
+        nbMovies = df_nb_films_acteurs[df_nb_films_acteurs['acteur'] == acteur].iloc[0, 1]
+        # On affiche une petite phrase (optionnel)
+        print('{} is an {}, born in {}.\n{} plays in {} movies, the most known are:'.format(actor_name, actor_prof1, actor_dates[0],
+                                                                             actor_name, nbMovies))
+        # Cherche le nom des films
+        for movie, i in zip(actor_movies, range(len(actor_movies))):
+                movies_title_list.append(df_films[['clientTitle']][df_films['imdbId'] == movie].iloc[0, 0])
+                print('- ', movies_title_list[i])
+        return actor_name, actor_dates, actor_prof, movies_title_list, nbMovies
 
-        print('{} is an {}, born in {}.'.format(actor_name, actor_prof1, actor_dates[0]))
-        return actor_name, actor_dates, actor_prof, actor_movies
 
-name, dates, prof, movies = actor_select()
-#%% TESTING ZONE
-moviesId = []
-movies_title_list = []
+name, dates, prof, movies, nbMovies = actor_select()
 
-for movie in movies:
-        movies_title_list.append(df_films[['title']][df_films['imdbId'] == movie].iloc[0, 0])
+#################### TEST ZONE ####################
+#%%
 
-print(movies_title_list)
