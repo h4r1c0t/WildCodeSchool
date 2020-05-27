83d31a8f9365dd7ddc29713cae429965dd49c7c1
h4r1c0t <vanstavel.sebastien@gmail.com>
2020-05-27 12:22:56 +0200

    Update ActorsData.py

--- Project/#3_movies_recommandations/Scripts/ActorsData.py
+++ Project/#3_movies_recommandations/Scripts/ActorsData.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from fuzzywuzzy import process
+
 import streamlit as st
 # from sklearn.neighbors import NearestNeighbors
 import requests
@@ -24,7 +26,7 @@ def acteurs_films() :
                     FROM imdb_title_principals itp
                     LEFT JOIN imdb_name_basic inb
                     ON itp.nconst = inb.nconst
-                    ''',conn)
+                    ''', conn)
 
         mask = df_acteurs['category'].isin(('actor', 'actress', 'self', 'archive_footage', 'archive_sound'))
         df_acteurs = df_acteurs[mask]
@@ -77,16 +79,56 @@ def acteurs():
                     FROM imdb_title_basics itb
                     ''', conn)
 
-
-
         return (df_acteurs)
+
 
 #%%
 df_acteurs = acteurs()
 
+#%% Création d'un DF avec les films
+def films():
+        df_films = pd.read_sql_query(
+                '''SELECT   m.movieId movieId, tab1.imdbId imdbId, m.title clientTitle, tab1.imdbTitle title, tab1.durée, m.genres genres
+                    FROM movies m 
+                    JOIN (SELECT l.movieId movieId, imdbId, itb.primaryTitle imdbTitle, startYear year, runtimeMinutes durée
+                           FROM imdb_title_basics itb
+                           JOIN links l
+                           ON l.imdbId = itb.tconst
+                           ) tab1
+                    ON tab1.movieId = m.movieId
+                    ''', conn)
+        return df_films
+
+
+df_films = films()
+
+#%% Fonction de sélection d'un acteur
+def actor_select(actor='Nicolas Cage'):
+        actors_list = list(df_acteurs['primaryName'])
+
+        # Cette boucle if permet de rechercher la bonne orthographe
+        if actor in actors_list:
+                pass
+        else:
+                actor = process.extractOne(actor, actors_list)[0]
+
+
+        actor_infos = df_acteurs[df_acteurs['primaryName'] == actor]
+        actor_name = actor_infos.iloc[0, 1]
+        actor_dates = actor_infos.iloc[0, 2:3]
+        actor_prof = actor_infos.iloc[0, 4].split(",")
+        actor_prof1 = actor_prof[0]
+        actor_movies = actor_infos.iloc[0, 5]
+
+        print('{} is an {}, born in {}.'.format(actor_name, actor_prof1, actor_dates[0]))
+        return actor_name, actor_dates, actor_prof, actor_movies
+
+name, dates, prof, movies = actor_select()
 #%% TESTING ZONE
+moviesId = []
+movies_title_list = []
+
+for movie in movies:
 
 
-actor = str(input())
-
-df_acteurs[df_acteurs['primaryName'] == actor]
+print(movies_title_list)
