9dab05056c2a316724683934648cae3365c82d70
h4r1c0t <vanstavel.sebastien@gmail.com>
2020-06-05 10:59:52 +0200

    Update the regresssion modeling

--- Odyssey/ModulesPerso/regression_modeling.py
+++ Odyssey/ModulesPerso/regression_modeling.py
@@ -1,16 +1,22 @@
-def Regressions_modelisation(model, data, target):
+def regression_modeling(model, data, target, screening):
+    '''
 
+    :param model: 'knn', 'lr', 'log'
+    :param data: DataFrame
+    :param target: y
+    :param screening: Do you want a print of the scores
+    :return: Return the fitted model and the columns used
+    '''
     ##### SELECTION DU TYPE DE REGRESSION
     # Select le linear model
-    if lower(model) == 'knn':
+    if model.lower() == 'knn':
         from sklearn.neighbors import KNeighborsRegressor
-    elif lower(model) == 'lr':
+    elif model.lower() == 'lr':
         from sklearn.linear_model import LinearRegression
-    elif lower(model) == 'log':
+    elif model.lower() == 'log':
         from sklearn.linear_model import LogisticRegression
     else:
         print('Error model type not valid')
-        break
 
     ##### IMPORT
     import numpy as np
@@ -34,34 +40,35 @@ def Regressions_modelisation(model, data, target):
 
     ##### BOUCLE D'ENTRAINEMENT DU MODEL
     # Variables
-      cols = []
-      train_score_max = 0
-      test_score_max = 0
-      Xcols = []
+    cols = []
+    score_max = 0
+    scores_max = 0
+    Xcolumns = []
 
     # Début de la boucle de modélisation en partant de la plus forte corrélation
     for x, r in dictCorr:
-      cols.append(x)    # Ajout d'une nouvelle variable au model
-      X = data[cols]
-      y = data[target]
+        cols.append(x)    # Ajout d'une nouvelle variable au model
+        X = data[cols]
+        y = data[target]
 
-        if lower(model) == 'knn':
+        if model.lower() == 'knn':
             cv =  StratifiedKFold(n_splits = 6, random_state = 42, shuffle = True)
             scores = cross_val_score(KNeighborsRegressor(), X, y, cv=cv,
                                         scoring = make_scorer(accuracy_score))
-        elif lower(model) == 'lr':
+        elif model.lower() == 'lr':
             cv =  StratifiedKFold(n_splits = 6, random_state = 42, shuffle = True)
             scores = cross_val_score(LinearRegression(), X, y, cv=cv,
                                     scoring = make_scorer(accuracy_score))
-        elif lower(model) == 'log':
+        elif model.lower(model) == 'log':
             cv =  StratifiedKFold(n_splits = 6, random_state = 42, shuffle = True)
             scores = cross_val_score(LogisticRegression(), X, y, cv=cv,
                                     scoring = make_scorer(accuracy_score))
         else:
             break
 
-        if mean(scores) > score_max:   # Si la nouvelle variable augmente le score,
-            score_max = mean(scores)     # on met à jour les variables
+        if np.mean(scores) > score_max:     # Si la nouvelle variable augmente le score,
+            score_max = np.mean(scores)     # on met à jour les variables
+            scores_max = scores
             Xcolumns = cols
         else:
             cols.remove(x)
@@ -77,21 +84,23 @@ def Regressions_modelisation(model, data, target):
       for col in Xcolumns:
         print("   ",col)
 
-      print('\nTrain score =', train_score_max)
-      print('Test score =', test_score_max)
+      print('\nMean score =', score_max)
+      print('Scores :', scores_max)
 
-  # Fit un nouveau model avec les meilleures variables
-  Xmax = data[Xcolumns]
-  X_train, X_test, y_train, y_test = train_test_split(Xmax, y, random_state=42,
+    # Fit un nouveau model avec les meilleures variables
+    from sklearn.model_selection import train_test_split
+    Xmax = data[Xcolumns]
+    X_train, X_test, y_train, y_test = train_test_split(Xmax, y, random_state=42,
                                                           train_size = 0.75)
-    if lower(model) == 'knn':
+    if model.lower() == 'knn':
         modelMax = KNeighborsRegressor().fit(X_train, y_train)
-    elif lower(model) == 'lr':
+    elif model.lower() == 'lr':
         modelMax = LinearRegression().fit(X_train, y_train)
-    elif lower(model) == 'log':
+    elif model.lower() == 'log':
         modelMax = LogisticRegression().fit(X_train, y_train)
     else:
         break
 
-  # Retourne le meilleur model.
-  return modelMax, Xcolumns
+    # Retourne le meilleur model.
+    return modelMax, Xcolumns
+
