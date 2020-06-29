91c9604ad9d43db0a88ac16b2b1e67cf3e5d6d92
h4r1c0t <sebastien-vanstavel_student2020@wilder.school>
2020-06-29 14:34:17 +0200

    datathon update

---
+++ Project/Datathon#2_Doctoiku/FLUX/INC_linearReg.py
@@ -0,0 +1,10 @@
+#%% import pandas as pd, numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv('D:/Github/Wild Code School/Project/Datathon#2_Doctoiku/GLOBAL_DATA_DROPNA_prepared.csv')
+
+#%%
+
+<img class="irc_mi" src="https://66.media.tumblr.com/8a1a4aa…/tumblr_mx4yr6rUM51si27z3o4_r1_250.gifv" data-atf="0" width="245" height="245" style>
