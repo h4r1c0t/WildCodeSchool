2eea22fe326089ee82d0783f67174943a2f4b70d
h4r1c0t <sebastien-vanstavel_student2020@wilder.school>
2020-07-01 22:55:51 +0200

    RS update

--- Project/RetailShake/Scripts/price_combine_distribution.py
+++ Project/RetailShake/Scripts/price_combine_distribution.py
@@ -1,13 +1,11 @@
 #%% Import
-# basics libraries
-import numpy as np
+# Basics libraries
 import pandas as pd
 
 # Plotly graphic libraries
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import plotly.io as pio
-from plotly.subplots import make_subplots
 
 pio.renderers.default = 'browser'
 
@@ -51,16 +49,9 @@ AS Price ON Product.`product-company-id` = Price.product_company_id ;
 """
 df_price = pd.read_sql(Query1, mydb)
 
-#%% Screen shape
+#%% Figure 1
 
-df_price.shape
-
-#%% Screen head
-
-df_price.head()
-
-#%% ff.distplot
-distPlt = ff.create_distplot([df_price["price_after"]], ['Price'],
+fig1 = ff.create_distplot([df_price["price_after"]], ['Price'],
                           bin_size = 10,
                           colors = ['#F25C05'],
                           rug_text = [['<b>{}</b> ' 
@@ -78,7 +69,7 @@ distPlt = ff.create_distplot([df_price["price_after"]], ['Price'],
                                                                                      df_price["change_date"])]]
                           )
 
-distPlt.update_layout(
+fig1.update_layout(
     title = {'text': 'Distribution des prix au sein de la gamme '
                      '<b>{}</b> de <b>{}</b>'.format(df_price.iloc[0, 1],
                                                      df_price.iloc[0, 0]),
@@ -94,38 +85,40 @@ distPlt.update_layout(
         color = "#7f7f7f")
 )
 
-distPlt.show()
+fig1.show()
 
-#%%
-histPlt = go.Figure()
+#%% Figure 2
 
-histPlt.add_trace(go.histogram(x = df_price['price_after']))
-histPlt.show()
+fig2 = go.Figure()
+fig2.add_trace(
+    go.Box(x = df_price['price_after'],
+           boxpoints = 'outliers',
+           jitter = .2,
+           pointpos = 0,
+           hovertemplate =
+           '<br>%{text}<br>',
+           text = ['<b>{}</b> ' 
+                   '<br><b>Price:</b> € {}'
+                   '<br><b>Gtin:</b> {}'
+                   '<br><b>Company:</b> {} <br><b>Update:</b> {}'.format(product,
+                                                                         price,
+                                                                         gtin,
+                                                                         name,
+                                                                         update)
+                   for product, price, gtin, name, update in zip(df_price.iloc[:, 2],
+                                                                 df_price["price_after"],
+                                                                 df_price["gtin"],
+                                                                 df_price["name"],
+                                                                 df_price["change_date"])],
+           name = 'Price',
+           opacity = 0.5,
+           marker_color = '#F2B705')
+)
 
-#%% Boxplot
-
-name = df_price['name']
-
-boxPlt = go.Figure(data = [go.Box(x = df_price['price_after'],
-                                  boxpoints = 'all',
-                                  jitter = .5,
-                                  pointpos = 2,
-                                  hovertemplate =
-                                  '<b>Price:</b> €%{x:.2f}' +
-                                  '<br>%{text}<br>',
-                                  text = ['<br><b>Name:</b> {} <br><b>Gtin:</b> {} '
-                                          '<br><b>Company:</b> {} <br><b>Update:</b> {}'.format(product,
-                                                                                                gtin,
-                                                                                                name,
-                                                                                                update)
-                                          for product, gtin, name, update in zip(df_price.iloc[:, 2],
-                                                                                 df_price["gtin"],
-                                                                                 df_price["name"],
-                                                                                 df_price["change_date"])],
-                                  name = 'Price',
-                                  marker_color='#F2B705')])
-boxPlt.update_layout(
-    title = {'text': 'Distribution des prix au sein de la gamme',
+fig2.update_layout(
+    title = {'text': 'Distribution des prix au sein de la gamme '
+                     '<b>{}</b> de <b>{}</b>'.format(df_price.iloc[0, 1],
+                                                     df_price.iloc[0, 0]),
              'y': .995,
              'x': .5,
              'xanchor': 'center',
@@ -137,42 +130,37 @@ boxPlt.update_layout(
         color = "#7f7f7f")
 )
 
-boxPlt.show()
+fig2.update_traces(orientation = 'h')
 
-#%% Subplots
+fig2.show()
 
-plts = make_subplots(
-    rows = 2, cols = 1,
-    row_heights = [.2, .8],
-    specs = [[{"type": "box"}],
-             [{"type": "histogram"}]]
-)
+#%% Edit the figures' x and y axes attributes to create subplots:
 
-plts.add_trace(
-    go.Box(y = df_price['price_after'],
-           boxpoints = 'all',
-           jitter = 0.3,
-           pointpos = -1.8,
-           hovertemplate =
-           '<b>Price:</b> €%{y:.2f}'+
-           '<br>%{text}<br>',
-           text = ['<br><b>Name:</b> {} <br><b>Gtin:</b> {} '
-                   '<br><b>Company:</b> {} <br><b>Update:</b> {}'.format(product,
-                                                                         gtin,
-                                                                         name,
-                                                                         update)
-                   for product, gtin, name, update in zip(df_price.iloc[:, 2],
-                                                          df_price["gtin"],
-                                                          df_price["name"],
-                                                          df_price["change_date"])],
-           name = 'Price',
-           marker_color='#F2B705'),
-    row = 1, col = 1
-)
+for i in range(len(fig1.data)):
+    fig1.data[i].xaxis = 'x2'
+    fig1.data[i].yaxis = 'y1'
 
-plts.add_trace(
-    go.histogram(x = [df_price["price_after"]]),
-    row = 2, col = 1
-)
+fig1.layout.xaxis1.update({'anchor': 'y1'})
+fig1.layout.yaxis1.update({'anchor': 'x2', 'domain': [.55, 1]})
 
-plts.show()
+for i in range(len(fig2.data)):
+    fig2.data[i].xaxis = 'x2'
+    fig2.data[i].yaxis = 'y2'
+
+# initialize xaxis2 and yaxis2
+fig2['layout']['xaxis2'] = {}
+fig2['layout']['yaxis2'] = {}
+
+fig2.layout.xaxis2.update({'anchor': 'y2'})
+fig2.layout.yaxis2.update({'anchor': 'x2', 'domain': [0, .45]})
+
+#%% Combine the data and layout objects to create a figure
+
+fig = go.Figure()
+fig.add_traces([fig1.data[0], fig2.data[0]])
+
+fig.layout.update(fig1.layout)
+fig.layout.update(fig2.layout)
+
+fig.show()
+
--- Project/RetailShake/Scripts/pricing_requests.sql
+++ Project/RetailShake/Scripts/pricing_requests.sql
@@ -1,34 +1,18 @@
-SELECT *
-FROM piwigo_retail_product_company
-WHERE `product-company-store-id` = "0000098024135000001";
-
-SELECT *
-FROM piwigo_retail_product_store
-WHERE `product-company-store-id` = "0000098024135000001";
-
-
--- Permet la sélection de la catégorie "SUSPENSION"
-SELECT label, cat_id
-FROM piwigo_retail_category_company
-WHERE label LIKE '%suspension%';
-
--- Permet de trouver tous les produits correspondants à la `cat_id`
-SELECT cat_id, `product-company-id`
-FROM piwigo_retail_product_company
-WHERE cat_id = "l1400351717";
-
--- Permet de sélectionner tous les prix enregistrés pour un produit
-SELECT `product_company_id`, price_after, change_date
-FROM piwigo_retail_product_company_price_changes
-WHERE product_company_id = 137130690002;
-
---
-SELECT `product_company_id`, price_after price, MAX(change_date) date
-FROM piwigo_retail_product_company_price_changes pcpc
-WHERE EXISTS (
-    SELECT `product-company-id`
-    FROM piwigo_retail_product_company
-    WHERE cat_id = "l1400351717" AND product_company_id = `product-company-id`
-    )
-GROUP BY product_company_id
-LIMIT 10;
+SELECT Company.name, Category.label, Product.label, Product.gtin, Product.`internal-reference`,
+    Price.price_before, Price.price_after, Price.change_date
+FROM piwigo_retail_product_company AS Product
+INNER JOIN
+    (SELECT id, name
+    FROM piwigo_retail_companies
+    WHERE name  = 'Leroy Merlin')
+AS Company ON  Product.`company-id` = Company.id
+INNER JOIN
+    (SELECT cat_id, label
+    FROM piwigo_retail_category_company
+    WHERE label LIKE '%suspension%')
+AS Category ON Product.cat_id = Category.cat_id
+INNER JOIN
+    (SELECT product_company_id, price_before, price_after, MAX(change_date) AS change_date
+    FROM piwigo_retail_product_company_price_changes
+    GROUP BY product_company_id, price_before, price_after)
+AS Price ON Product.`product-company-id` = Price.product_company_id ;
